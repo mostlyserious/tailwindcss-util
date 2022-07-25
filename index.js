99ecc4826d16cf8cccb3d68a7255211401271b73
Cornelius Ukena <cornelius@mostlyserious.io>
2022-07-25 08:36:59 -0500

    🚀 convert to npm package

---
+++ index.js
@@ -0,0 +1,13 @@
+const clamp = require('./plugin/clamp');
+const inset = require('./plugin/inset');
+const viewport = require('./plugin/viewport');
+const xallarap = require('./plugin/xallarap');
+const textStroke = require('./plugin/text-stroke');
+const stateVariant = require('./lib/state-variant');
+
+exports.clamp = clamp;
+exports.inset = inset;
+exports.viewport = viewport;
+exports.xallarap = xallarap;
+exports.textStroke = textStroke;
+exports.stateVariant = stateVariant;
