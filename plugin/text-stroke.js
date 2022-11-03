6516a6ff761477841822e6a58a4506b5853cf985
Cornelius Ukena <cornelius@mostlyserious.io>
2022-11-03 09:52:56 -0500

    ✨ improved text-stroke plugin

--- plugin/text-stroke.js
+++ plugin/text-stroke.js
@@ -1,6 +1,14 @@
 const plugin = require('tailwindcss/plugin');
+const { default: withAlphaVariable } = require('tailwindcss/lib/util/withAlphaVariable');
+const { default: flattenColorPalette } = require('tailwindcss/lib/util/flattenColorPalette');
 
-module.exports = plugin(({ matchUtilities, addComponents, addUtilities, theme }) => {
+module.exports = plugin(({ matchUtilities, addBase, addComponents, addUtilities, theme }) => {
+    addBase({
+        '*, ::before, ::after': {
+            '-webkit-text-stroke-color': 'currentcolor'
+        }
+    });
+
     matchUtilities({
         'text-stroke': value => ({
             '-webkit-text-stroke-width': value
@@ -11,11 +19,15 @@ module.exports = plugin(({ matchUtilities, addComponents, addUtilities, theme })
     });
 
     matchUtilities({
-        'text-stroke': value => ({
-            '-webkit-text-stroke-color': value
-        })
+        'text-stroke': value => {
+            return withAlphaVariable({
+                color: value,
+                property: '-webkit-text-stroke-color',
+                variable: '--tw-text-stroke-opacity'
+            });
+        }
     }, {
         type: 'color',
-        values: theme('colors')
+        values: (({ DEFAULT: _, ...colors }) => colors)(flattenColorPalette(theme('borderColor')))
     });
 });
